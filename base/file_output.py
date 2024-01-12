9ee80757826d3266b2cf5d789870069bf20373e9
simonge <simon.gardner@glasgow.ac.uk>
2024-01-12 15:32:53 +0000

    fixed hepmc output close

--- base/file_output.py
+++ base/file_output.py
@@ -103,6 +103,7 @@ class file_output:
             print("HepMC3 root output name:", nam)
 
             self.hepmc_root_out = hmrootIO.WriterRootTree(nam)
+            atexit.register(self.close_hepmc_root)
 
             
         self.hepmc_ievt = 0
@@ -254,7 +255,7 @@ class file_output:
         #vertex loop
         for v in vertices:
             evt.add_vertex(v)
-
+        
         if self.set_write_hepmc:
             self.hepmc_out.write_event(evt)
             
